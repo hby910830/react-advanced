feb8db472cb36626fa97f8338a2b1fada60baae3
hanbaoyi <604594231@qq.com>
2019-12-14 23:28:55 +0800

    自己实现一个useState

--- hooks-theory/src/index.js
+++ hooks-theory/src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-function App(){
-	console.log('app运行了')  //说明点击按钮setN会再次调用app()，重新渲染组建
-	const [n, setN] = React.useState(0)
+
+let _state //全局_state用来存储state的值，避免重新渲染的时候被myUseState重置为初始值
+const myUseState = initialValue => {
+	_state = _state === undefined ? initialValue : _state
+	const setState = (newValue) => {
+		_state = newValue
+		render()
+	}
+	return [_state, setState]
+}
+
+const render = () => {
+	ReactDOM.render(<App1 />, document.getElementById('root'))
+}
+
+function App1(){
+	const [n, setN] = myUseState(0)
 	return (
 		<div className='App'>
 			<p>{n}</p>
@@ -12,4 +26,17 @@ function App(){
 		</div>
 	)
 }
-ReactDOM.render(<App />, document.getElementById('root'))
+
+// function App(){
+// 	console.log('app运行了')  //说明点击按钮setN会再次调用app()，重新渲染组建
+// 	const [n, setN] = React.useState(0)
+// 	return (
+// 		<div className='App'>
+// 			<p>{n}</p>
+// 			<p>
+// 				<button onClick={() => setN(n + 1)}>+1</button>
+// 			</p>
+// 		</div>
+// 	)
+// }
+ReactDOM.render(<App1 />, document.getElementById('root'))
