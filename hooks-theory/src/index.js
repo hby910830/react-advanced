10c653a4a59a42bb2016b7e1245c615e78d64385
hanbaoyi <604594231@qq.com>
2019-12-14 23:51:31 +0800

    解决一个组件内使用多个useState的bug

--- hooks-theory/src/index.js
+++ hooks-theory/src/index.js
@@ -1,27 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-let _state //全局_state用来存储state的值，避免重新渲染的时候被myUseState重置为初始值
+let _state = [] //全局_state用来存储state的值，避免重新渲染的时候被myUseState重置为初始值
+let index = 0
 const myUseState = initialValue => {
-	_state = _state === undefined ? initialValue : _state
-	const setState = (newValue) => {
-		_state = newValue
+	const currentIndex = index
+	_state[currentIndex] = _state[currentIndex] === undefined ? initialValue : _state[currentIndex]
+	const setState = newValue => {
+		_state[currentIndex] = newValue
 		render()
 	}
-	return [_state, setState]
+	index += 1
+	return [_state[currentIndex], setState]
 }
 
 const render = () => {
+	index = 0
 	ReactDOM.render(<App1 />, document.getElementById('root'))
 }
 
 function App1(){
 	const [n, setN] = myUseState(0)
+	const [m, setM] = myUseState(0)
 	return (
 		<div className='App'>
 			<p>{n}</p>
 			<p>
 				<button onClick={() => setN(n + 1)}>+1</button>
+			</p>
+			<p>{m}</p>
+			<p>
+				<button onClick={() => setM(m + 1)}>+1</button>
 			</p>
 		</div>
 	)
