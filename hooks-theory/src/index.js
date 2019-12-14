c5df5dcc823a53333f2547917495749a6efcbec8
hanbaoyi <604594231@qq.com>
2019-12-14 23:05:42 +0800

    setN会重新渲染组建

--- hooks-theory/src/index.js
+++ hooks-theory/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 function App(){
+	console.log('app运行了')  //说明点击按钮setN会再次调用app()，重新渲染组建
 	const [n, setN] = React.useState(0)
 	return (
 		<div className='App'>
