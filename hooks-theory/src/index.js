758074c1b81a7da2e42fddd4e1cf2803851768de
hanbaoyi <604594231@qq.com>
2019-12-14 23:00:30 +0800

    useState基本用法

--- hooks-theory/src/index.js
+++ hooks-theory/src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 function App(){
-	return <div>hi</div>
+	const [n, setN] = React.useState(0)
+	return (
+		<div className='App'>
+			<p>{n}</p>
+			<p>
+				<button onClick={() => setN(n + 1)}>+1</button>
+			</p>
+		</div>
+	)
 }
 ReactDOM.render(<App />, document.getElementById('root'))
