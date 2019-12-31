51d2a10cabffd1b48d89c914092082158f1108bf
hanbaoyi <604594231@qq.com>
2019-12-31 23:40:28 +0800

    useReducer(reducer, initial)

--- hooks-explain/use-reducer/src/index.js
+++ hooks-explain/use-reducer/src/index.js
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initial = {n: 0};
+const reducer = (state, action) => {
+	if (action.type === "add") {
+		return {n: state.n + action.number};
+	} else if (action.type === "multi") {
+		return {n: state.n * 2};
+	} else {
+		throw new Error("unknown type");
+	}
+};
+
+function App() {
+	const [state, dispatch] = useReducer(reducer, initial);
+	const {n} = state
+	const onClick = () => {
+		// dispatch({type: "add", number: 1});
+	};
+	return (
+		<div className="App">
+			<h1>n: {n}</h1>
+			<button onClick={onClick}>+2</button>
+		</div>
+	);
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App/>, rootElement);
