911f77dee18451720d52f52c9f0759dacd141b72
hanbaoyi <604594231@qq.com>
2020-01-03 00:27:58 +0800

    useLayoutEffect 和 useEffect 执行顺序

--- hooks-explain/use-layout-effect/src/index.js
+++ hooks-explain/use-layout-effect/src/index.js
@@ -16,20 +16,20 @@ function App() {
 	);
 }
 
-function App1(){
+function App1() {
 	const [n, setN] = useState(0)
 	const time = useRef(null)
-	const onClick = ()=>{
-		setN(i=>i+1)
+	const onClick = () => {
+		setN(i => i + 1)
 		time.current = performance.now()
 	}
-	useLayoutEffect(()=>{ 
-		if(time.current){
+	useLayoutEffect(() => {
+		if (time.current) {
 			console.log(performance.now() - time.current) //大概是0.7ms
 		}
 	})
-	useEffect(()=>{
-		if(time.current){
+	useEffect(() => {
+		if (time.current) {
 			console.log(performance.now() - time.current) //大概是2.7ms
 		}
 	})
@@ -40,4 +40,29 @@ function App1(){
 		</div>
 	);
 }
-ReactDOM.render(<App1/>, document.getElementById('root'));
+
+/* useLayoutEffect比useEffect先执行 */
+function App2() {
+	const [n, setN] = useState(0)
+	const onClick = () => {
+		setN(i => i + 1)
+	}
+	//执行顺序打印出 2、3、1
+	useEffect(() => {
+		console.log(1)
+	})
+	useLayoutEffect(() => {
+		console.log(2)
+	})
+	useLayoutEffect(() => {
+		console.log(3)
+	})
+	return (
+		<div className="App">
+			<h1>n: {n}</h1>
+			<button onClick={onClick}>Click</button>
+		</div>
+	);
+}
+
+ReactDOM.render(<App2/>, document.getElementById('root'));
