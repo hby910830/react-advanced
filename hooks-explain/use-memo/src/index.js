310a74eec0fc82a6769ed92734e3bc5ec855dedd
hanbaoyi <604594231@qq.com>
2020-01-05 23:55:41 +0800

    React.memo

--- hooks-explain/use-memo/src/index.js
+++ hooks-explain/use-memo/src/index.js
@@ -15,18 +15,15 @@ function App() {
 				<button onClick={onClick}>update n {n}</button>
 			</div>
 			<Child data={m}/>
-			{/* <Child2 data={m}/> */}
 		</div>
 	);
 }
 
-function Child(props) {
-	console.log("child 执行了");
-	console.log('假设这里有大量代码')
-	return <div>child: {props.data}</div>;
-}
-
-const Child2 = React.memo(Child);
+const Child = React.memo(props => {
+		console.log("child 执行了");
+		console.log('假设这里有大量代码')
+		return <div>child: {props.data}</div>;
+});
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
