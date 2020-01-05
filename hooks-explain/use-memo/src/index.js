2a49d32f687de1f6e035fdef3d9621ce57159533
hanbaoyi <604594231@qq.com>
2020-01-06 00:03:12 +0800

    React.memo使用引用时有bug

--- hooks-explain/use-memo/src/index.js
+++ hooks-explain/use-memo/src/index.js
@@ -7,22 +7,24 @@ function App() {
 	const onClick = () => {
 		setN(n + 1);
 	};
-
+	const onClickChild = () => {}
 	return (
 		<div className="App">
 			<div>
 				{/*点击button会重新执行Child组件*/}
 				<button onClick={onClick}>update n {n}</button>
 			</div>
-			<Child data={m}/>
+			{/*但是如果传了一个引用，则React.memo无效。因为引用是不相等的*/}
+			<Child data={m} onClick={onClickChild}/>
 		</div>
 	);
 }
 
+//使用React.memo可以解决重新执行Child组件的问题
 const Child = React.memo(props => {
 		console.log("child 执行了");
 		console.log('假设这里有大量代码')
-		return <div>child: {props.data}</div>;
+		return <div onClick={props.onClick}>child: {props.data}</div>;
 });
 
 const rootElement = document.getElementById("root");
