c5b234a37495b19550db6d10860e3343279bf56e
hanbaoyi <604594231@qq.com>
2020-01-06 00:28:03 +0800

    useMemo

--- hooks-explain/use-memo/src/index.js
+++ hooks-explain/use-memo/src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useMemo} from "react";
 import ReactDOM from "react-dom";
 
 function App() {
@@ -7,15 +7,26 @@ function App() {
 	const onClick = () => {
 		setN(n + 1);
 	};
+	const onClick1 = () => {
+		setM(m + 1);
+	};
 	const onClickChild = () => {}
+	const onClickChild1 = useMemo(() => {
+		return () => {
+			console.log(`on click child m: ${m}`)
+		}
+	}, [m])
 	return (
 		<div className="App">
 			<div>
 				{/*点击button会重新执行Child组件*/}
 				<button onClick={onClick}>update n {n}</button>
+				<button onClick={onClick1}>update m {m}</button>
 			</div>
 			{/*但是如果传了一个引用，则React.memo无效。因为引用是不相等的*/}
-			<Child data={m} onClick={onClickChild}/>
+			{/*<Child data={m} onClick={onClickChild}/>*/}
+			{/*onClickChild1使用useMemo可以消除此bug*/}
+			<Child data={m} onClick={onClickChild1}/>
 		</div>
 	);
 }
